966947c41911c54e187236734ecf5a30cdbd0a51
super887 <suwer887@gmail.com>
2017-03-10 12:14:01 +0900

    5

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^keyboard', 'kookmingo.views.keyboard'),
+    url(r'^keyboard', include('kookmingo.urls')),
     url(r'^admin/', admin.site.urls),
 ]
