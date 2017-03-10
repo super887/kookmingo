31d2a1d914a7b21e5e165b0002a314863c1aded4
super887 <suwer887@gmail.com>
2017-03-10 14:43:52 +0900

    8

--- kookmingo/models.py
+++ kookmingo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+class Menu(models.Model):
+    id=models.AutoField(primary_key=True)
+    cafe_name = models.CharField(max_length=30,default="")
+    time = models.CharField(max_length=30,default="")
+    menu = models.CharField(max_length=100,default="")
+
 
 # Create your models here.
--- kookmingo/views.py
+++ kookmingo/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .models import Menu
 import json, datetime
 from django.views.decorators.csrf import csrf_exempt
-
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
 def keyboard(request):
     return JsonResponse(
         {
         'type':'buttons',
-        'buttons':['상록','그루터기']
+        'buttons':['복지관(학식)','복지관(교직원)','법식']
     })
 
 @csrf_exempt
@@ -19,12 +21,41 @@ def answer(request):
 
     return JsonResponse({
         'message':{
-            'text':today_date + 'of' + cafeteria_name +'middle'
+            'text':today_date + ' 의 ' + cafeteria_name +'중식 메뉴입니다.\n \n'+ get_menu(cafeteria_name)
         },
         'keyboard':{
             'type':'buttons',
-            'buttons':['상록','그루터기']
+            'buttons': ['복지관(학식)', '복지관(교직원)', '법식']
         }
     })
 
+def crawl(request):
+    menu_db = Menu.objects.all()
+    menu_db.delete()
+
+    html= urlopen('http://kmucoop.kookmin.ac.kr/restaurant/restaurant.php?w=2')
+    source = html.read()
+    html.close()
+
+    soup = BeautifulSoup(source,"lxml")
+
+    table = soup.find_all("td", class_="ft_mn")
+    Menu.menu = table[0]
+    return create_menu_db('복지관','11',Menu.menu)
+
+
+def create_menu_db(cafe_name,time,menu):
+    Menu.objects.create(
+        cafe_name = cafe_name,
+        time = time,
+        menu = menu,
+    )
+
+def get_menu(cafeteria_name):
+    if cafeteria_name == '복지관(학식)':
+        sang1 = Menu.objects.get(cafe_name='백반코너').menu
+        sang2 = Menu.objects.get(cafe_name='일품코너').menu
+
+        return "----------\n" + "백반코너\n" +sang1 \
+                +"----------\n" + "일품코너\n" +sang2\
 # Create your views here.
