1f58e0149d1b79fe1e0569ddce78d7fc2f621415
super887 <suwer887@gmail.com>
2017-03-13 13:57:06 +0900

    12

--- kookmingo/views.py
+++ kookmingo/views.py
@@ -18,11 +18,11 @@ def answer(request):
     json_str = ((request.body).decode('utf-8'))
     received_json_data = json.loads(json_str)
     cafeteria_name = received_json_data['content']
-    today_date = datetime.date.today().strftime("%m월 %d일")
+    today_date = datetime.date.today().strftime("%m월 %d일s")
 
     return JsonResponse({
         'message':{
-            'text':today_date + ' 의 ' + cafeteria_name +'중식 메뉴입니다.\n \n'+ get_menu(cafeteria_name)
+            'text':today_date + ' 의 ' + cafeteria_name +'중식 메뉴입니다.\n \n'
         },
         'keyboard':{
             'type':'buttons',
