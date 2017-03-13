1cc35c8cace068ce7a8753c13ec811fd175905ef
super887 <suwer887@gmail.com>
2017-03-13 13:45:15 +0900

    10

--- kookmingo/models.py
+++ kookmingo/models.py
@@ -4,7 +4,7 @@ class Menu(models.Model):
     id=models.AutoField(primary_key=True)
     cafe_name = models.CharField(max_length=30,default="")
     time = models.CharField(max_length=30,default="")
-    menu = models.CharField(max_length=100,default="")
+    menu = models.CharField(max_length=3000,default="")
 
 
 # Create your models here.
