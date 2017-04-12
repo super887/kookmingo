024cac0eb53c2585c1238ac287c2b81deb05698e
super887 <suwer887@gmail.com>
2017-04-12 15:11:37 +0900

    la

--- myproject/settings.py
+++ myproject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'kookmingo',
-    'kookmingohomepage.apps.KookmingohomepageConfig'
+    'kookmingohomepage',
 ]
 
 MIDDLEWARE = [
